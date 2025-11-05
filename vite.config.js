e330c866bcd68fbac7333155d666d4970ac83407
Devsphz <devsphz@gmail.com>
2025-11-05 16:20:33 -0300

    Fix: dist

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        sobre: 'sobre.html',
+        contato: 'contato.html',
+      },
+    },
+  },
+});
